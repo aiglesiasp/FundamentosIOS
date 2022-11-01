2125e904daaaf4c9bed7a418a0de0f0f385a3d1c
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-11-01 17:38:57 +0100

    First commit
    Login View Creado

---
+++ 0-DragonBallApp/0-DragonBallApp/Controllers/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  0-DragonBallApp
+//
+//  Created by Aitor Iglesias Pubill on 1/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    //Linkeo los componenetes de la View
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    
+    //Carga de la pantalla
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
